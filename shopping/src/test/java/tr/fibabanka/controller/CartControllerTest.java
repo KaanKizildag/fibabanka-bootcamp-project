618725692c36cef18d1e7f7fcfa5337479b97faf
hkaankizildag <hkizildag@partner.innova.com.tr>
2022-11-09 14:22:19 +0300

    CartProductControllerTest | eklendi

--- shopping/src/test/java/tr/fibabanka/controller/CartControllerTest.java
+++ shopping/src/test/java/tr/fibabanka/controller/CartControllerTest.java
@@ -1,16 +1,27 @@
 package tr.fibabanka.controller;
 
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import tr.fibabanka.dto.CartDto;
 import tr.fibabanka.service.CartService;
+
+import java.util.ArrayList;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(MockitoExtension.class)
 class CartControllerTest {
@@ -32,8 +43,43 @@ class CartControllerTest {
 
     @Test
     @DisplayName("Sepet oluşturulabilmelidir")
-    void createCart() {
-
+    void createCart() throws Exception {
+        mockMvc.perform(get("/shopping/cart/create/huseyin")
+                        .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isOk())
+                .andDo(print());
     }
+
+    @Test
+    @DisplayName("Sepet durumu güncellenebilmelidir.")
+    void checkout() throws Exception {
+        Long cartId = 1L;
+
+        mockMvc.perform(get("/shopping/checkout/" + cartId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("Sepet Id ile sorgulanabilmelidir.")
+    void findCartById() throws Exception {
+        Long cartId = 1L;
+
+        String customerName = "Hüseyin";
+        Mockito.when(cartService.findById(cartId))
+                .thenReturn(new CartDto(1L, customerName, new ArrayList<>()));
+
+        mockMvc.perform(get("/shopping/cart/find/" + cartId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.customerName", Matchers.is(customerName)))
+                .andExpect(jsonPath("$.id", Matchers.is(cartId.intValue())))
+                .andDo(print());
+    }
+
 
 }
---
+++ shopping/src/test/java/tr/fibabanka/controller/CartProductControllerTest.java
@@ -0,0 +1,79 @@
+package tr.fibabanka.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import tr.fibabanka.dto.CartProductDto;
+import tr.fibabanka.service.CartProductService;
+
+import java.math.BigDecimal;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(MockitoExtension.class)
+class CartProductControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Mock
+    private CartProductService cartProductService;
+
+    @InjectMocks
+    private CartProductController cartProductController;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders.standaloneSetup(cartProductController)
+                .build();
+    }
+
+    @SneakyThrows
+    @Test
+    @DisplayName("Sepete ürün eklenebilmelidir.")
+    void addToCart() {
+        CartProductDto cartProductDto = getMockCartProductDto();
+
+        Mockito.when(cartProductService.addProductToCart(Mockito.any()))
+                .thenReturn(cartProductDto);
+
+        mockMvc.perform(post("/shopping/cart/add")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(cartProductDto))
+                )
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+
+    private CartProductDto getMockCartProductDto() {
+        return new CartProductDto(1L, 1L, 1L, 5, BigDecimal.TEN, BigDecimal.ZERO);
+    }
+
+    @SneakyThrows
+    @Test
+    @DisplayName("Sepetten ürün silinebilmelidir.")
+    void deleteProductFromCart() {
+        long cartId = 1L;
+        long productId = 1L;
+
+        mockMvc.perform(delete("/shopping//cart/" + cartId + "/remove/" + productId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+}
